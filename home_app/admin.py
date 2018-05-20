47955de4b3716b0c965d5fe78804db406f24fc22
Demir Antay <demir99antay@gmail.com>
2018-05-20 21:54:17 +0300

    added an office model

--- home_app/admin.py
+++ home_app/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Announcament
+from .models import Announcament, Office, UserAdittion
 
 admin.site.register(Announcament)
+admin.site.register(Office)
+admin.site.register(UserAdittion)
--- home_app/models.py
+++ home_app/models.py
@@ -2,16 +2,43 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 from profile_app.models import ProfileSettings
+import random
+import string
+
+
+# FUNCTIONS FOR MODELS
+def generate_secret_key():
+    return ''.join(
+       random.choice(string.ascii_uppercase + string.digits) for _ in range(20)
+    )
 
 
 # USER ADITTIONS
 class UserAdittion(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    joined_office = models.OneToOneField("Office", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return user.username
 
 
 # OFFICE
 class Office(models.Model):
-    pass
+    admin = models.OneToOneField(User, on_delete=models.CASCADE)
+    admin_settings = models.ForeignKey(
+            ProfileSettings, on_delete=models.CASCADE
+    )
+    admin_adittions = models.ForeignKey(UserAdittion, on_delete=models.CASCADE)
+    name = models.CharField(max_length=250)
+    TYPE_CHOICES = (
+        ("Journalism", "Journalism"),
+    )
+    type = models.CharField(max_length=250, choices=TYPE_CHOICES)
+    secret_key = models.CharField(max_length=20, default=generate_secret_key)
+    creation_date = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return self.office_name
 
 
 # ANNOUNCAMENTS
